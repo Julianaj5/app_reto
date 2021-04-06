64ff2cbc355636f4db70aa5409fda07349588c66
Karen Gómez <28kjuliana@gmail.com>
2021-04-06 17:46:12 -0500

    login

---
+++ android/app/src/main/kotlin/com/example/app_reto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_reto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:app_reto/src/ui/email_page.dart';
+
+import 'package:flutter/material.dart';
+
+
+
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Google Sign In',
+      home: LoginPage(),
+    ),
+  );
+}
